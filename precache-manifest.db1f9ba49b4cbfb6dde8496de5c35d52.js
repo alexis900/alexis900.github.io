0615146fef42b035a24433ddba64ce1aad7fa9dc
Alejandro Martin Perez <alexandermpf77@gmail.com>
2019-12-22 00:52:58 +0100

    first commit

---
+++ precache-manifest.db1f9ba49b4cbfb6dde8496de5c35d52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d390a68de187e3f3494068ef8dee936",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c39d64a7d44997a5e26",
+    "url": "./static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "35a1b46b9b87c879747d",
+    "url": "./static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0c39d64a7d44997a5e26",
+    "url": "./static/js/2.7b375841.chunk.js"
+  },
+  {
+    "revision": "9682237fa99bc1a358b1ac2de59fd99c",
+    "url": "./static/js/2.7b375841.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35a1b46b9b87c879747d",
+    "url": "./static/js/main.f7e878c4.chunk.js"
+  },
+  {
+    "revision": "14831b585184cf85fe08",
+    "url": "./static/js/runtime-main.2ed94321.js"
+  }
+]);
