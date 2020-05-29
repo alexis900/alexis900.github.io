e2af20efb5dd1728e4ac4ae2bd20aae326a38979
Alejandro Martín Pérez <alexandermpf77@gmail.com>
2020-05-30 01:57:27 +0200

    first commit 🚀

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+function calculateAge(birthday) {
+    var birthday_arr = birthday.split("/");
+    var birthday_date = new Date(birthday_arr[2], birthday_arr[1] - 1, birthday_arr[0]);
+    var ageDifMs = Date.now() - birthday_date.getTime();
+    var ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+document.getElementById("age").innerHTML = calculateAge("22/11/1998");
